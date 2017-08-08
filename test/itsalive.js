ae5a4f5a9250059da3282c5d3d76fd79620e48ed
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-08-08 12:16:52 -0400

    excercises

---
+++ test/itsalive.js
@@ -0,0 +1,40 @@
+const expect = require('chai').expect
+const chai = require('chai');
+const spies = require('chai-spies');
+chai.use(spies);
+//const supertest = require('supertest')
+// const agent = supertest.agent(app);
+
+// describe('addition', function(){
+//   function addition(num1, num2){
+//     return num1 + num2;
+//   }
+//   it('adds 2 and 2', function(){
+//     let sum = addition(2, 2);
+//     expect(sum).to.equal(4);
+//   })
+// }); 
+
+// describe('using Done to wait for it block', function(){
+//   it('make sure setTimeout took 1000ms', function(done){
+//     var start = new Date(); 
+//     setTimeout(function (){
+//       var duration = new Date() - start; 
+//       expect(duration).to.be.closeTo(1000, 50);
+//       done();
+//     }, 1000);
+//   })
+// }) 
+
+// describe('how spies work', function(){
+//   it('spy on the number of times forEach ran', function(){
+//     let testArr = ['x', 'y', 'z'];
+//     function logNth(val, idx){
+//       console.log(`Logging elem #${idx}:${val}`);
+//     }
+//     logNth = chai.spy(logNth);
+//     testArr.forEach(logNth); 
+//     expect(logNth).to.have.been.called.exactly(testArr.length);
+//   })
+// }); 
+
